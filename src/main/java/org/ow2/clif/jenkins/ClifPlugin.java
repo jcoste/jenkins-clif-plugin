8246da36de7a1b152c6b4d6b29c0faf505b6cac8
Julien <jcosteguard-github@yahoo.fr>
2012-04-04 12:09:05 +0200

    Add clif root workspace configuration

--- src/main/java/org/ow2/clif/jenkins/ClifPlugin.java
+++ src/main/java/org/ow2/clif/jenkins/ClifPlugin.java
@@ -21,18 +21,71 @@
 package org.ow2.clif.jenkins;
 
 import hudson.Plugin;
+import hudson.model.Descriptor;
+import hudson.model.Hudson;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.StaplerRequest;
+
+import javax.servlet.ServletException;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Entry point for the Clif plugin.
  *
  * @author Julien Coste
  */
-public class ClifPlugin
-		extends Plugin {
+public class ClifPlugin extends Plugin {
 
 	static final String ICON_FILE_NAME = "graph.gif";
 
 	static final String DISPLAY_NAME = Messages.Plugin_DisplayName();
 
 	static final String URL = "clif";
+
+	/**
+	 * Root directory for storing Clif projects.
+	 */
+	private String clifRootDir = "clif";
+
+
+	@Override
+	public void configure(StaplerRequest req, JSONObject formData)
+			throws IOException, ServletException, Descriptor.FormException {
+		clifRootDir = formData.getString("clifRootDir").trim();
+		save();
+	}
+
+	/**
+     * @return The configured Clif root directory.
+     */
+	public String getClifRootDir() {
+		return clifRootDir;
+	}
+
+	public void setClifRootDir(String clifRootDir) {
+		this.clifRootDir = clifRootDir;
+	}
+
+	/**
+     * Returns the File object representing the configured clif root directory.
+     *
+     * @return The configured clif root File object, or <JENKINS_ROOT>/clif  if this configuration has not been set.
+     */
+    protected File getConfiguredClifRootDir() {
+        File rootFile = null;
+        if (StringUtils.isNotBlank(clifRootDir)) {
+            if (clifRootDir.matches("^(/|\\\\|[a-zA-Z]:).*")) {
+                rootFile = new File(clifRootDir);
+            } else {
+                rootFile = new File(Hudson.getInstance().root.getPath() + "/" + clifRootDir);
+            }
+        }
+	    else {
+	        rootFile = new File(Hudson.getInstance().root.getPath() + "/clif" );
+        }
+        return rootFile;
+    }
+
 }
---
+++ src/test/java/org/ow2/clif/jenkins/ClifPluginTest.java
@@ -0,0 +1,85 @@
+package org.ow2.clif.jenkins;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.jvnet.hudson.test.HudsonTestCase;
+
+import java.io.File;
+
+public class ClifPluginTest extends HudsonTestCase {
+
+	private WebClient webClient;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+/* En commentaire tant que Clif-core embarque un Xalan 2.5.1
+		hudson.setSecurityRealm(new HudsonPrivateSecurityRealm(true));
+		webClient = createWebClient();
+*/
+	}
+
+	@Test
+	public void testGetClifRootDirAbsolutePath() throws Exception {
+		final ClifPlugin clifPlugin = hudson.getPlugin(ClifPlugin.class);
+		try {
+			// create a unique name, then delete the empty file - will be recreated later
+			final File root = File.createTempFile("clifPlugin.test_abs_path", null);
+			final String absolutePath = root.getPath();
+			root.delete();
+
+
+/* En commentaire tant que Clif-core embarque un Xalan 2.5.1
+
+			final HtmlForm form = webClient.goTo("configure").getFormByName("config");
+			form.getInputByName("clifRootDir").setValueAttribute(absolutePath);
+			submit(form);
+
+*/
+			// En attendant, on change le test
+			clifPlugin.setClifRootDir(absolutePath);
+			assertEquals("Verify clif root configured at absolute path.", root, clifPlugin.getConfiguredClifRootDir());
+
+			root.delete();
+			// not really needed, but helpful so we don't clutter the test host with unnecessary files
+			assertFalse("Verify cleanup of history files: " + root, root.exists());
+
+		}
+		catch (Exception e) {
+			fail("Unable to complete clif root absolute path test: " + e);
+		}
+	}
+
+	@Test
+	public void testGetClifRootDirRelativePath() throws Exception {
+		final ClifPlugin clifPlugin = hudson.getPlugin(ClifPlugin.class);
+		try {
+			final String relativePath = "clifPlugin.test_rel_path";
+			final File root = new File(hudson.root.getPath()+File.separator+relativePath);
+			root.delete();
+
+/* En commentaire tant que Clif-core embarque un Xalan 2.5.1
+
+			final HtmlForm form = webClient.goTo("configure").getFormByName("config");
+			form.getInputByName("clifRootDir").setValueAttribute(relativePath);
+			submit(form);
+
+*/
+			// En attendant, on change le test
+			clifPlugin.setClifRootDir(relativePath);
+			assertEquals("Verify clif root configured at relative path.", root, clifPlugin.getConfiguredClifRootDir());
+
+		}
+		catch (Exception e) {
+			fail("Unable to complete clif root absolute path test: " + e);
+		}
+	}
+
+	@Test
+	public void testJobConfigHistoryDefaults() {
+		final ClifPlugin clifPlugin = hudson.getPlugin(ClifPlugin.class);
+
+		Assert.assertNotNull("Bad default clif root dir", clifPlugin.getClifRootDir());
+		Assert.assertEquals("Bad default clif root dir", "clif", clifPlugin.getClifRootDir());
+	}
+}
